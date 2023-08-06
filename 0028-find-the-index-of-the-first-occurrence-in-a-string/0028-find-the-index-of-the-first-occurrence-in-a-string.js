5577fc2f3242296aad68e9c4b35717a652a46bc3
Aria Fikri N S <111510916+AriaFikri@users.noreply.github.com>
2023-08-06 09:56:16 +0700

    Time: 59 ms (50.75%), Space: 41.8 MB (69.50%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  let left = haystack.indexOf(needle[0]);
+  let right = left + needle.length;
+  let solution = haystack.slice(left, right);
+
+  for (let i = 0; i <= haystack.length; i++) {
+    solution = haystack.slice(left, right);
+    if (solution === needle) return left;
+    right++;
+    left++;
+  }
+  if (solution === needle) return left
+  else return -1;
+};
